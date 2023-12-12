b8e2b4ab54866691e71f7796a08e72915e0ba6e0
noahkise <noah@kisefamily.com>
2023-12-12 09:18:42 -0800

    initial commit, add diagram and header
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+import '../App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+    <Header />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <React.Fragment>
+        <h1>Not Wordle</h1>
+        </React.Fragment>
+    )
+}
+export default Header;
