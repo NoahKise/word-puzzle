601eee04427034e3ba7c724ad79c974987e14f38
noahkise <noah@kisefamily.com>
2023-12-13 12:00:50 -0800

    lunch
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com>

--- src/components/EndScenario.js
+++ src/components/EndScenario.js
@@ -3,16 +3,23 @@ import PropTypes from 'prop-types';
 
 const EndScenario = (props) => {
     let message;
+    let correctWord;
     if (props.endState === true) {
         message = "Congrats You Win!";
     } else {
         message = "Sorry Try Again";
+        correctWord = (
+            <React.Fragment>
+                The word you were looking for was&nbsp; 
+                <em>{props.unsolved.join("")}</em>
+            </React.Fragment>
+        );
     }
     
     return(
         <React.Fragment>
             <h3>{message}</h3>
-            <h4>The word you were looking for was <em>{props.unsolved}</em></h4>
+            <h4>{correctWord}</h4>
             <button onClick={props.click}>Play again</button> 
         </React.Fragment>
     )
