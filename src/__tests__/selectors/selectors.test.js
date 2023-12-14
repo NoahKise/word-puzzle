e7be64963b4683086f8dd62c1043aa4adad0b7a5
noahkise <noah@kisefamily.com>
2023-12-13 16:12:10 -0800

    update selector test state, all pass
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com>

--- src/__tests__/selectors/selectors.test.js
+++ src/__tests__/selectors/selectors.test.js
@@ -9,66 +9,50 @@ import { inGameSelector } from '../../redux/inGameSlice';
 import { wrongGuessesSelector } from '../../redux/wrongGuessesSlice';
 
 describe('Selector tests', () => {
+    const state = {
+        answer: ["C", "A", "T"],
+        correctGuesses: ["C"],
+        displayAnswer: ["C", "A", "T"],
+        endState: false,
+        error: 'You can not enter a number',
+        gameOver: false,
+        guessesRemaining: 6,
+        inGame: true,
+        wrongGuesses: ["Q", "W"],
+    };
     it('should show state is selected correctly for answerSelector', () => {
-        const state = {
-            answer: ["C", "A", "T"],
-        };
         const selectedAnswer = answerSelector(state);
         expect(selectedAnswer).toEqual(["C", "A", "T"]);
     });
     it('should show state is selected correctly for correctGuessesSelector', () => {
-        const state = {
-            correctGuesses: ["C"],
-        };
         const selectedAnswer = correctGuessesSelector(state);
         expect(selectedAnswer).toEqual(["C"]);
     });
     it('should show state is selected correctly for displayAnswerSelector', () => {
-        const state = {
-            displayAnswer: ["C", "A", "T"],
-        };
         const selectedAnswer = displayAnswerSelector(state);
         expect(selectedAnswer).toEqual(["C", "A", "T"]);
     });
     it('should show state is selected correctly for endStateSelector', () => {
-        const state = {
-            endState: false,
-        };
         const selectedAnswer = endStateSelector(state);
         expect(selectedAnswer).toEqual(false);
     });
     it('should show state is selected correctly for errorSelector', () => {
-        const state = {
-            error: 'You can not enter a number',
-        };
         const selectedAnswer = errorSelector(state);
         expect(selectedAnswer).toEqual('You can not enter a number');
     });
     it('should show state is selected correctly for gameOverSelector', () => {
-        const state = {
-            gameOver: false,
-        };
         const selectedAnswer = gameOverSelector(state);
         expect(selectedAnswer).toEqual(false);
     });
     it('should show state is selected correctly for guessesRemainingSelector', () => {
-        const state = {
-            guessesRemaining: 6,
-        };
         const selectedAnswer = guessesRemainingSelector(state);
         expect(selectedAnswer).toEqual(6);
     });
     it('should show state is selected correctly for inGameSelector', () => {
-        const state = {
-            inGame: true,
-        };
         const selectedAnswer = inGameSelector(state);
         expect(selectedAnswer).toEqual(true);
     });
     it('should show state is selected correctly for wrongGuessesSelector', () => {
-        const state = {
-            wrongGuesses: ["Q", "W"],
-        };
         const selectedAnswer = wrongGuessesSelector(state);
         expect(selectedAnswer).toEqual(["Q", "W"]);
     });
